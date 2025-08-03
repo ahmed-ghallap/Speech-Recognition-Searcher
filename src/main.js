f9c93a86f383943f04c6e924668a666281adcddd
ahmed-ghallap <ahmedor759@gmail.com>
2025-08-04 02:35:06 +0300

    foo

--- src/main.js
+++ src/main.js
@@ -4,29 +4,54 @@ class SpeechRecognitionSearcher {
   constructor() {
     this.micIcon = document.querySelector("svg");
     this.recognition = null;
+    this.triggerElement = document.querySelector("#trigger");
     this.isListening = false;
-    this.textElement = document.querySelector("p");
+    this.textPlaceholder = document.createElement("p");
+    this.textContainer = document.querySelector(".content");
+    this.textContainer.appendChild(this.textPlaceholder);
     this.errorElement = document.querySelector(".err");
     this.init();
+    this.setup();
+  }
+  start() {
+    this.recognition.start();
   }
 
-  init() {
-    if ("SpeechRecognition" in window || "webkitSpeechRecognition" in window) {
-      this.recognition = new (window.SpeechRecognition ||
-        window.webkitSpeechRecognition)();
-      this.recognition.lang = "en-US";
-      this.recognition.interimResults = true;
-    } else {
-      this.errorElement.classList.remove("hidden");
-      this.micIcon.querySelector("line").classList.remove("hidden");
-      console.error("SpeechRecognition API is not supported in this browser.");
-      return;
-    }
-    this.recognition.start();
+  changeIcon(className) {
+    this.micIcon.querySelectorAll("*").forEach((el) => {
+      el.classList.add("hidden");
+    });
+    this.micIcon.querySelector("." + className).classList.remove("hidden");
+  }
+  setup() {
+    this.recognition.interimResults = true;
+    this.recognition.onend = (event) => {
+      this.recognition.start();
+    };
+    this.recognition.onresult = (event) => {
+      // makes it more smoother with fast talk
+      const transcript = Array.from(event.results)
+        .map((result) => result[0].transcript)
+        .join("");
+
+      this.textPlaceholder.innerText = transcript;
+
+      if (event.results[0].isFinal) {
+        this.isListening = false;
+        this.textPlaceholder = document.createElement("p");
+        this.textContainer.appendChild(this.textPlaceholder);
+        this.changeIcon("mic");
+        this.recognition.stop();
+      } else {
+        this.isListening = true;
+        this.changeIcon("hearing");
+      }
+    };
+
     this.recognition.onstart = () => {
       this.isListening = true;
-      this.micIcon.classList.add("listening");
-      this.textElement.textContent = "Listening...";
+      this.changeIcon("hearing");
+      // this.textPlaceholder.textContent = "Listening...";
     };
 
     this.recognition.onerror = (event) => {
@@ -34,9 +59,27 @@ class SpeechRecognitionSearcher {
       this.textElement.textContent =
         "Error occurred in recognition: " + event.error;
       this.isListening = false;
-      this.micIcon.classList.remove("listening");
+      this.changeIcon("mic-error");
+    };
+  }
+
+  init() {
+    if ("SpeechRecognition" in window || "webkitSpeechRecognition" in window) {
+      this.recognition = new (window.SpeechRecognition ||
+        window.webkitSpeechRecognition)();
+      // this.recognition.lang = "en-US";
+      this.recognition.lang = "ar-EG";
+      this.recognition.interimResults = true;
+    } else {
+      this.errorElement.classList.remove("hidden");
+      this.micIcon.querySelector("line").classList.remove("hidden");
+      console.error("SpeechRecognition API is not supported in this browser.");
+      return;
+    }
+    this.triggerElement.onclick = () => {
+      this.start();
     };
   }
 }
 
-new SpeechRecognitionSearcher();
+let foo = new SpeechRecognitionSearcher();
